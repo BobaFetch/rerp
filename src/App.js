57d59a8dc18edc59b4663e6ba0ac312c6401e022
bobaFetch <thejza@Andrews-MacBook-Air.local>
2022-05-15 17:45:31 -0700

    changed layout

--- src/App.js
+++ src/App.js
@@ -6,29 +6,28 @@ import Home from './components/views/Home';
 import Administration from './components/views/Administration';
 import Production from './components/views/Production';
 import Engineering from './components/views/Engineering';
-import Finance from './components/views/Finance'
+import Finance from './components/views/Finance';
 
 const viewMap = {
   Home,
   Administration,
   Production,
   Engineering,
-  Finance
-}
-
+  Finance,
+};
 
 export default function App() {
-  const [title, setTitle] = useState('Home')
-  const Component = viewMap[title]
+  const [title, setTitle] = useState('Home');
+  const Component = viewMap[title];
   return (
-    <div className='container-full h-screen bg-gradient-to-br from-slate-900 to-cyan-500 overflow-y-hidden'>
+    <div className="container-full h-screen bg-gradient-to-br from-slate-900 to-cyan-500 overflow-y-hidden">
       <Header name={title} />
-      <div className='grid grid-cols-10 gap-2 text-white h-5/6'>
-        <div className='col-span-1'>
+      <div className="grid grid-cols-10 gap-1 text-white h-5/6">
+        <div className="col-span-1">
           <SideNav setTitle={setTitle} />
         </div>
-        <div className='col-span-9 px-2 bg-gray-200 text-black mr-2 rounded overflow-auto'>
-         <Component />
+        <div className="col-span-9 px-2 bg-gray-200 text-black mr-2 rounded overflow-auto">
+          <Component />
         </div>
       </div>
     </div>
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,40 +1,84 @@
-import { useState } from 'react'
-import FeatherIcon from 'feather-icons-react'
-import Button from "./Button"
-
-
+import { useState } from 'react';
+import FeatherIcon from 'feather-icons-react';
+import Button from './Button';
 
 export default function SideNav(props) {
-    const [screen, setScreen] = useState('Home')
+  const [screen, setScreen] = useState('Home');
 
-    function handleScreen(v) {
-        // e.preventDefault()
-        setScreen(v)
-        props.setTitle(v)
-    }
+  function handleScreen(v) {
+    // e.preventDefault()
+    setScreen(v);
+    props.setTitle(v);
+  }
 
-    return (
-        <div className="grid grid-cols-2 gap-2 pl-2">
-            <Button onClick={() => handleScreen('Home')} title='Home'>
-                <FeatherIcon icon='home' size='50' strokeWidth='1' className={screen === 'Home' ? 'mx-auto text-red-500' : 'mx-auto text-gray-600'} />
-                HOME
-            </Button>
-            <Button onClick={() => handleScreen('Administration')} title='Administration'>
-                <FeatherIcon icon='users' size='50' strokeWidth='1' className={screen === 'Administration' ? 'mx-auto text-red-500' : 'mx-auto text-gray-600'} />
-                ADMIN
-            </Button>
-            <Button onClick={() => handleScreen('Production')} title='Production'>
-              <FeatherIcon icon='book-open' size='50' strokeWidth='1' className={screen === 'Production' ? 'mx-auto text-red-500' : 'mx-auto text-gray-600'} />
-              PRODUCTION
-            </Button>
-            <Button onClick={() => handleScreen('Engineering')} title='Engineering'>
-              <FeatherIcon icon='tool' size='50' strokeWidth='1' className={screen === 'Engineering' ? 'mx-auto text-red-500' : 'mx-auto text-gray-600'} />
-              ENGINEERING
-            </Button>
-            <Button onClick={() => handleScreen('Finance')} title='Finance'>
-              <FeatherIcon icon='dollar-sign' size='50' strokeWidth='1' className={screen === 'Finance' ? 'mx-auto text-red-500' : 'mx-auto text-gray-600'} />
-              FINANCE
-            </Button>
-        </div>
-    )
+  return (
+    <div className="grid grid-cols-1 justify-items-center gap-2 pl-2">
+      <Button onClick={() => handleScreen('Home')} title="Home">
+        <FeatherIcon
+          icon="home"
+          size="50"
+          strokeWidth="1"
+          className={
+            screen === 'Home' ? 'mx-auto text-red-500' : 'mx-auto text-gray-600'
+          }
+        />
+        HOME
+      </Button>
+      <Button
+        onClick={() => handleScreen('Administration')}
+        title="Administration"
+      >
+        <FeatherIcon
+          icon="users"
+          size="50"
+          strokeWidth="1"
+          className={
+            screen === 'Administration'
+              ? 'mx-auto text-red-500'
+              : 'mx-auto text-gray-600'
+          }
+        />
+        ADMIN
+      </Button>
+      <Button onClick={() => handleScreen('Production')} title="Production">
+        <FeatherIcon
+          icon="book-open"
+          size="50"
+          strokeWidth="1"
+          className={
+            screen === 'Production'
+              ? 'mx-auto text-red-500'
+              : 'mx-auto text-gray-600'
+          }
+        />
+        PRODUCTION
+      </Button>
+      <Button onClick={() => handleScreen('Engineering')} title="Engineering">
+        <FeatherIcon
+          icon="tool"
+          size="50"
+          strokeWidth="1"
+          className={
+            screen === 'Engineering'
+              ? 'mx-auto text-red-500'
+              : 'mx-auto text-gray-600'
+          }
+        />
+        ENGINEERING
+      </Button>
+      <Button onClick={() => handleScreen('Finance')} title="Finance">
+        <FeatherIcon
+          icon="dollar-sign"
+          size="50"
+          strokeWidth="1"
+          className={
+            screen === 'Finance'
+              ? 'mx-auto text-red-500'
+              : 'mx-auto text-gray-600'
+          }
+        />
+        FINANCE
+      </Button>
+    </div>
+  );
 }
